5c61b2b44ca3734d3a6737ab08e6a325f137a4c0
Thales Gomes <thaleshng@gmail.com>
2024-05-31 19:33:34 -0300

    mudando algumas coisas nos arquivos css e js

--- src/js/index.js
+++ src/js/index.js
@@ -1,18 +1,3 @@
-// const images = document.querySelectorAll(".image")
-// const buttons = document.querySelectorAll(".button")
-
-// buttons.forEach((button, index) => {
-//     button.addEventListener("click", () => {
-//         const selectedButton = document.querySelector(".selected")
-//         selectedButton.classList.remove("selected")
-//         button.classList.add("selected")
-
-//         const activeImage = document.querySelector(".active")
-//         activeImage.classList.remove("active")
-//         images[index].classList.add("active")
-//     })
-// })
-
 const images = document.querySelectorAll(".image")
 const buttons = document.querySelectorAll(".button")
 
